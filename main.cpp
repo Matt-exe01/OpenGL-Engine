80abc26265e9c6ecc3da0eb40b31b16a8b192b51
Matteo Dallaserra <matteo.dallaserra@marconirovereto.it>
2023-02-14 19:38:42 +0100

    Aggiunta classe camera

--- main.cpp
+++ main.cpp
@@ -1,15 +1,39 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "shader/shaderHandler.h"
+#include "camera/camera.h"
 
 #include <iostream>
+
 
 
 Shader* shaderManager;
 
 bool wireframe = false;
 
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = 1920 / 2.0f;
+float lastY = 1080 / 2.0f;
+bool firstMouse = true;
+
+
+
 void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+
+// timing
+float deltaTime = 0.0f;	//tempo tra il frame corrente e quello passato
+float lastFrame = 0.0f;
 
 int main()
 {
@@ -32,6 +56,7 @@ int main()
     glfwMakeContextCurrent(window);
     glfwMaximizeWindow(window);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
 
     // Carico la robaccia di glad
     // ---------------------------------------
@@ -41,35 +66,123 @@ int main()
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     shaderManager = new Shader("./shader/shaders/basicVert.glsl", "./shader/shaders/basicFrag.glsl");
-
-
 
 
     //========== TMP - Test Only ==========\\
 
     float vertices[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 1.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+        //BACK
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //0
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f, //1
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, //2
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, //2
+         0.5f, -0.5f, -0.5f, 1.0f, 0.0f, //3
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //0
+        //FRONT
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, //6
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, //4
+         0.5f, -0.5f,  0.5f, 1.0f, 0.0f, //7
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f, //4
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, //6
+        -0.5f,  0.5f,  0.5f, 0.0f, 1.0f, //5
+        //BOTTOM
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //0
+         0.5f, -0.5f, -0.5f, 1.0f, 0.0f, //3
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, //7
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, //7
+        -0.5f, -0.5f,  0.5f, 0.0f, 1.0f, //4 
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //0
+        //TOP
+        -0.5f,  0.5f, -0.5f, 1.0f, 0.0f, //6
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, //2
+         0.5f,  0.5f,  0.5f, 0.0f, 1.0f, //1
+         0.5f,  0.5f,  0.5f, 0.0f, 1.0f, //1
+         0.5f,  0.5f, -0.5f, 0.0f, 0.0f, //5
+        -0.5f,  0.5f, -0.5f, 1.0f, 0.0f, //6
+        //LEFT
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, //5
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f, //1
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //0
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //0
+        -0.5f, -0.5f,  0.5f, 1.0f, 0.0f, //4
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, //5
+        //RIGHT
+         0.5f, -0.5f, -0.5f, 1.0f, 0.0f, //3
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, //2
+         0.5f,  0.5f,  0.5f, 0.0f, 1.0f, //6
+         0.5f,  0.5f,  0.5f, 0.0f, 1.0f, //6
+         0.5f, -0.5f,  0.5f, 0.0f, 0.0f, //7
+         0.5f, -0.5f, -0.5f, 1.0f, 0.0f, //3
+
+
     };
+
+    // posizione nel mondo dei miei cubi
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
+    //Creo le OpenGL texture
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+
+    //Prendo la texture con STB_Image
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("./res/skin.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        //TIPO, MIPMAP, COLORI, WIDTH. HEIGHT, LEGACY, COLORI, DATA TYPE, DATA
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        std::cout << "ok";
+    }
+
+    //Libero la memoria dall'immagine
+    stbi_image_free(data);
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    // Attributi positione
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    // Attributi colore
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Unbindo il VAO
+    glBindVertexArray(0);
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
 
     //========== End TMP - Test Only ==========\\
 
@@ -79,6 +192,14 @@ int main()
     // -----------
     while (!glfwWindowShouldClose(window))
     {
+
+        // logica per frame time (in base al frame time decido poi quanto muovere la camera)
+        // --------------------
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+
         // input
         // -----
         processInput(window);
@@ -86,12 +207,32 @@ int main()
         // render
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+
+        shaderManager->setInt("texture", 0);
         shaderManager->use();
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
 
+
+
+        glm::mat4 projection = glm::perspective(glm::radians(60.0f), (float)1920 / (float)1080, 0.1f, 100.0f);
+        shaderManager->setMat4("projection", projection);
+
+        glm::mat4 view = glm::lookAt(camera.Position, camera.Position + camera.Front, camera.Up);
+        shaderManager->setMat4("view", view);
+
+
+        glBindVertexArray(VAO);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            //model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            shaderManager->setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         // Swappa i buffer e mette in coda gli eventi
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
@@ -120,4 +261,47 @@ void processInput(GLFWwindow* window)
         wireframe = false;
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     }
+
+
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        camera.ProcessKeyboard(UP, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+        camera.ProcessKeyboard(DOWN, deltaTime);
+    }
+        
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // rovesciato, le coordinate vanno da bottom a top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
 }
--- shader/shaderHandler.h
+++ shader/shaderHandler.h
@@ -1,8 +1,8 @@
-#pragma once
 #ifndef SHADER_H
 #define SHADER_H
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 #include <string>
 #include <fstream>
@@ -13,44 +13,43 @@ class Shader
 {
 public:
     unsigned int ID;
-    // Costruttore - Genera le shader al volo
+    // constructor generates the shader on the fly
     // ------------------------------------------------------------------------
     Shader(const char* vertexPath, const char* fragmentPath)
     {
-        // 1. prendo i file path
+        // 1. retrieve the vertex/fragment source code from filePath
         std::string vertexCode;
         std::string fragmentCode;
         std::ifstream vShaderFile;
         std::ifstream fShaderFile;
-        // abilito il throwing delle eccezioni
+        // ensure ifstream objects can throw exceptions:
         vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
         fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
         try
         {
-            // apro i file
+            // open files
             vShaderFile.open(vertexPath);
             fShaderFile.open(fragmentPath);
             std::stringstream vShaderStream, fShaderStream;
-            // metto i buffer nello stream
+            // read file's buffer contents into streams
             vShaderStream << vShaderFile.rdbuf();
             fShaderStream << fShaderFile.rdbuf();
-            // chiudo i file manager
+            // close file handlers
             vShaderFile.close();
             fShaderFile.close();
-            // converto lo stream in stringa
+            // convert stream into string
             vertexCode = vShaderStream.str();
             fragmentCode = fShaderStream.str();
         }
         catch (std::ifstream::failure& e)
         {
-            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: " << e.what() << std::endl;
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
         }
         const char* vShaderCode = vertexCode.c_str();
         const char* fShaderCode = fragmentCode.c_str();
-        // 2. compilo le shader
-        unsigned int vertex, fragment = 0;
+        // 2. compile shaders
+        unsigned int vertex, fragment;
         // vertex shader
-        std::cout << &vShaderCode << "\n" << &vertex;
         vertex = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertex, 1, &vShaderCode, NULL);
         glCompileShader(vertex);
@@ -60,23 +59,24 @@ public:
         glShaderSource(fragment, 1, &fShaderCode, NULL);
         glCompileShader(fragment);
         checkCompileErrors(fragment, "FRAGMENT");
-        // programma shader
+        // shader Program
         ID = glCreateProgram();
         glAttachShader(ID, vertex);
         glAttachShader(ID, fragment);
         glLinkProgram(ID);
         checkCompileErrors(ID, "PROGRAM");
-        // cancello per liberare spazio in memoria (sono già linkate)
+        // delete the shaders as they're linked into our program now and no longer necessary
         glDeleteShader(vertex);
         glDeleteShader(fragment);
+
     }
-    // attivo le shader
+    // activate the shader
     // ------------------------------------------------------------------------
-    void use()
+    void use() const
     {
         glUseProgram(ID);
     }
-    // utility uniform
+    // utility uniform functions
     // ------------------------------------------------------------------------
     void setBool(const std::string& name, bool value) const
     {
@@ -92,14 +92,56 @@ public:
     {
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
     }
+    // ------------------------------------------------------------------------
+    void setVec2(const std::string& name, const glm::vec2& value) const
+    {
+        glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+    void setVec2(const std::string& name, float x, float y) const
+    {
+        glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+    }
+    // ------------------------------------------------------------------------
+    void setVec3(const std::string& name, const glm::vec3& value) const
+    {
+        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+    void setVec3(const std::string& name, float x, float y, float z) const
+    {
+        glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+    }
+    // ------------------------------------------------------------------------
+    void setVec4(const std::string& name, const glm::vec4& value) const
+    {
+        glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+    void setVec4(const std::string& name, float x, float y, float z, float w) const
+    {
+        glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
+    }
+    // ------------------------------------------------------------------------
+    void setMat2(const std::string& name, const glm::mat2& mat) const
+    {
+        glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+    // ------------------------------------------------------------------------
+    void setMat3(const std::string& name, const glm::mat3& mat) const
+    {
+        glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+    // ------------------------------------------------------------------------
+    void setMat4(const std::string& name, const glm::mat4& mat) const
+    {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
 
 private:
-    // funzioni di logging errori private
+    // utility function for checking shader compilation/linking errors.
     // ------------------------------------------------------------------------
-    void checkCompileErrors(unsigned int shader, std::string type)
+    void checkCompileErrors(GLuint shader, std::string type)
     {
-        int success;
-        char infoLog[1024];
+        GLint success;
+        GLchar infoLog[1024];
         if (type != "PROGRAM")
         {
             glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
